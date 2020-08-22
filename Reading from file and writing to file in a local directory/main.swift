a1da7266f02ffec5970678e48e6282c59d0af064
Danya Khrapovitckii <40688428+danyaffff@users.noreply.github.com>
2020-08-22 15:52:39 +0300

    Delete main.swift

--- Reading from file and writing to file in a local directory/main.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-let inputURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent("input.txt")
-let outputURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent("output.txt")
-
-let text = try String(contentsOf: inputURL, encoding: .utf8)
-
-try text.write(to: outputURL, atomically: true, encoding: .utf8)
